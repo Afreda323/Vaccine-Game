5dbbcab88c26092a19c6467d0a25ace9941b0756
Anthony Freda <anthonyfreda323@gmail.com>
2020-03-27 15:03:06 -0400

    weapon firing

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public GameObject particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(particles, transform.position, Quaternion.identity);
+    }
+}
