0202df145420e0d05c2f60c203ccedaa8bccfc43
Anthony Freda <anthonyfreda323@gmail.com>
2020-03-27 14:27:38 -0400

    Player and weapon scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public Animator animator;
+
+    Rigidbody2D rb;
+    Vector2 moveAmount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (moveInput != Vector2.zero)
+        {
+            animator.SetBool("isRunning", true);
+        } else
+        {
+            animator.SetBool("isRunning", false);
+        }
+        moveAmount = moveInput.normalized * speed;
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
     }
 }
